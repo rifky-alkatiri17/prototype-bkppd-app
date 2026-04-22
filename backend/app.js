719b5a98cb81ec1c46b93746f237dcea82fe88f8
rifky-alkatiri17 <rifkyalkatiri17@gmail.com>
2026-04-22 16:15:51 +0800

    mengubah file app pada bagian backend

--- backend/app.js
+++ backend/app.js
@@ -4,23 +4,48 @@ const csvToJson = require("convert-csv-to-json");
 const fs = require("fs");
 
 const app = express();
+
+//config
+app.use(express.static('public'));
 const upload = multer({ dest: "uploads/" });
 
 app.post("/upload", upload.single("file"), (req, res) => {
-  try {
-    const filePath = req.file.path;
+    try {
+        console.log(req.file);
+        const filePath = req.file.path;
 
-    const json = csvToJson.getJsonFromCsv(filePath);
+        const json = csvToJson.fieldDelimiter(';').getJsonFromCsv(filePath);
 
-    fs.unlinkSync(filePath); // hapus file setelah dipakai
+        fs.unlinkSync(filePath); // hapus file setelah dipakai
 
-    res.json({
-      success: true,
-      data: json
-    });
-  } catch (err) {
-    res.status(500).json({ error: "Gagal convert CSV" });
-  }
+        /*res.json({
+            success: true,
+            data: json
+        });*/
+
+         // tampilkan + link download
+        res.send(`
+          <h2>Hasil Convert JSON</h2>
+          <pre>${JSON.stringify(json, null, 2)}</pre>
+          <a href="/download?file=${outputFile}">Download JSON</a>`);
+
+
+    } catch (err) {
+        res.status(500).json({ error: "Gagal convert CSV" });
+    }
 });
 
-app.listen(3001, () => console.log("Server jalan di 3001"));
+//route
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+    // res.sendFile(__dirname + '/public/coba.html')
+});
+
+// route download
+app.get('/download', (req, res) => {
+    console.log(req.query);
+    const file = req.query.file;
+    res.download(file);
+});
+
+app.listen(3001, () => console.log(`Server jalan di https://localhost:3001`));
